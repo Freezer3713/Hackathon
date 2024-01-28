43ac05a5c50849ce5a6f92a5efbb72e303f7d550
afnan-rah <128552429+afnan-rah@users.noreply.github.com>
2024-01-28 04:18:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,34 @@
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
-from functions_all import get_course_info, print_course_info, get_grade_data, prof_rating
+from all_functions import get_course_info, get_grade_data, prof_rating
 
-import pprint
-
-app = Flask(__name__)
+app = Flask(__name__, static_folder="./static")
 CORS(app)
 
 @app.route('/')
 def index():
-    
     return render_template('Hackathon.html')
 
-
-@app.route('/api/prof_rating/<prof_name>')
+@app.route('/api/prof_rating/<prof_name>', methods=['GET'])
 def api_prof_rating(prof_name):
-    print(prof_name)
-    #data = request.json
-    '''
-    print('======')
-    pprint.pprint(data)
-    prof_name = data['prof_name']
+    # Assuming prof_name is provided as a URL parameter
     result = prof_rating(prof_name)
     return jsonify(result)
-    '''
-    return prof_name
 
 @app.route('/api/get_grade_data', methods=['POST'])
 def api_get_grade_data():
     data = request.json
-    course_name = data['course_name']
+    course_name = data.get('course_name')  # Adjust based on how you send data from frontend
     result = get_grade_data(course_name)
     return jsonify(result)
 
 @app.route('/api/get_course_info', methods=['POST'])
 def api_get_course_info():
     data = request.json
-    subject = data['subject']
-    class_code = data['class_code']
+    subject = data.get('subject')  # Adjust based on how you send data from frontend
+    class_code = data.get('class_code')  # Adjust based on how you send data from frontend
     result = get_course_info(subject, class_code)
     return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
